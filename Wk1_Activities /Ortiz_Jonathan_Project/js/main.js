b1a00261eba0e534a3833faee853716b97be2977
Jon1367 <jonathano1367@yahoo.com>
2014-08-18 19:49:16 -0700

    changes

--- Wk1_Activities /Ortiz_Jonathan_Project/js/main.js
+++ Wk1_Activities /Ortiz_Jonathan_Project/js/main.js
@@ -54,8 +54,36 @@
 
 	})
 
-	
-
 
 
 })(jQuery);
+
+$('.masterTooltip').hover(function(){
+
+	var title = $(this).attr('title');
+	$(this).data('tipText', title).removeAttr('title');
+	$('<p class="tooltip"></p>')
+	.text(title)
+	.appendTo('body')
+	.fadeIn('slow');
+
+}, function() {
+
+	$(this).attr('title', $(this).data('tipText'));
+	$('.tooltip').remove();
+}).mousemove(function(e){
+
+	var mousex = e.pageX + 20;
+	var mousey = e.pageY + 10;
+	$('.tooltip')
+	.css({top: mousey, left:mousex})
+
+});
+$('.masterTooltip').hover(function(){
+
+
+	$("#signinButton").active("background-color","red");
+
+
+
+}) 
