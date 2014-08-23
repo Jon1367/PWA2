9cb735f58d18b49c614cff0c620709e13ae93195
Jon1367 <jonathano1367@yahoo.com>
2014-08-22 19:57:52 -0700

    work :/

---
+++ Wk3_Activities/Ortiz_Jonathan_Project/js/main.js
@@ -0,0 +1,115 @@
+/*======== tooltip =========*/
+
+(function($){
+
+
+	$('.Mclick').click(function(event){
+		event.preventDefault();
+		$('#back')
+			.fadeIn()
+			.find('#modal')
+			.fadeIn();
+	})
+
+	$(".close").click(function(event){
+		event.preventDefault();
+		$('#back')
+			.fadeOut()
+			.find('#modal')
+			.fadeOut();
+
+	})
+
+	$('ul.nav').each(function(){
+
+
+		var $on, $content ,$buttons = $(this).find('a');
+
+		$on = $($buttons.filter('[href="'+location.hash+'"]')[0] || $buttons[0]);
+
+		$on.addClass('active');
+
+		$content = $($on[0].hash);
+
+		$buttons.not($on).each(function(){
+
+			$(this.hash).hide();
+		});
+
+		$(this).on('click', 'a', function(e){
+
+			$on.removeClass('active');
+			$content.hide();
+
+			$on=$(this);
+			$content=$(this.hash);
+
+
+			$on.addClass('active');
+			$content.show();
+
+			e.preventDefault;
+
+		})
+
+	})
+
+	$('#signinButton').click(function(){
+
+		var user = $('#user').val();
+		var pass = $('#pass').val();
+
+		console.og("This notifies you if the passwrod is working");
+		$.ajax({
+
+			url:"xhr/login.php",
+			type:"post",
+			dataType:"json",
+			data:{
+
+				username: user,
+				password: pass
+			},
+			success:function(response){
+				console.log("test User");
+				if(response.error){
+					alert(response.error);
+				}else{
+					window.location.assign("Admin.html")
+				};
+			}
+		});
+	});
+
+
+})(jQuery);
+
+$('.masterTooltip').hover(function(){
+
+	var title = $(this).attr('title');
+	$(this).data('tipText', title).removeAttr('title');
+	$('<p class="tooltip"></p>')
+	.text(title)
+	.appendTo('body')
+	.fadeIn('slow');
+
+}, function() {
+
+	$(this).attr('title', $(this).data('tipText'));
+	$('.tooltip').remove();
+}).mousemove(function(e){
+
+	var mousex = e.pageX + 20;
+	var mousey = e.pageY + 10;
+	$('.tooltip')
+	.css({top: mousey, left:mousex})
+
+});
+$('.masterTooltip').hover(function(){
+
+
+	$("#signinButton").active("background-color","red");
+
+
+}) 
+
